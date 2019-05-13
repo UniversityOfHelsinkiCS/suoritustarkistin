637ac8f65a2211965f2d6b876846afca08a9314b
Jere Helén <jere.helen@helsinki.fi>
2019-05-13 15:37:35 +0300

    Add axios and simple course fetch

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("it's alive")
+require('dotenv').config()
+
+const axios = require('axios')
+
+const courseIds = axios
+  .get('https://www.avoin.helsinki.fi/eduweb/api/?course=AYTKT21018', {
+    headers: {
+      Authorized: process.env.EDUWEB_TOKEN
+    }
+  })
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message))
+
+//console.log(courseIds)

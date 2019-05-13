b0f8a80f3a2ecf7b4993a37e8b6f39ab8b9e7baa
Tapio <tgtapio@lm9-500-017.local>
2019-05-13 17:20:08 +0300

    adds method to get all participants by course id, extracts url

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const axios = require('axios')
 
+const eduwebUrl = 'https://www.avoin.helsinki.fi/eduweb/api/?course='
+
 const courseIds = axios
-  .get('https://www.avoin.helsinki.fi/eduweb/api/?course=AYTKT21018', {
+  .get(`${eduwebUrl}AYTKT21018`, {
     headers: {
       Authorized: process.env.EDUWEB_TOKEN
     }
@@ -11,4 +13,13 @@ const courseIds = axios
   .then((result) => console.log(result))
   .catch((error) => console.log(error.message))
 
-//console.log(courseIds)
+const courseParticipants = courseId => axios
+  .get(`${eduwebUrl}${courseId}`, {
+    headers: {
+      Authorized: process.env.EDUWEB_TOKEN
+    }
+  })
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message))
+
+courseParticipants(31897)

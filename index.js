48223b1ca9ea31b0df097a11ba79313268db3c72
Jere Helén <jere.helen@helsinki.fi>
2019-05-14 09:05:26 +0300

    switch to async-await

--- index.js
+++ index.js
@@ -2,24 +2,43 @@ require('dotenv').config()
 
 const axios = require('axios')
 
-const eduwebUrl = 'https://www.avoin.helsinki.fi/eduweb/api/?course='
+const courseCodes = ['AYTKT21018']
 
-const courseIds = axios
-  .get(`${eduwebUrl}AYTKT21018`, {
-    headers: {
-      Authorized: process.env.EDUWEB_TOKEN
+const getRegistrations = async (course) => {
+  const { data } = await axios.get(
+    `https://www.avoin.helsinki.fi/eduweb/api/?course=${course}`,
+    {
+      headers: {
+        Authorized: process.env.EDUWEB_TOKEN
+      }
     }
-  })
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error.message))
+  )
+  const instances = data.map((instance) => instance.url)
 
-const courseParticipants = courseId => axios
-  .get(`${eduwebUrl}${courseId}`, {
-    headers: {
-      Authorized: process.env.EDUWEB_TOKEN
-    }
-  })
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error.message))
+  let participants = []
+  for (const instance of instances) {
+    const { data } = await axios.get(
+      `https://www.avoin.helsinki.fi/eduweb/api/?course=${instance}`,
+      {
+        headers: {
+          Authorized: process.env.EDUWEB_TOKEN
+        }
+      }
+    )
+    participants = participants.concat(data)
+  }
+  console.log(`Found ${participants.length} registrations for ${course}`)
+  return participants
+}
 
-courseParticipants(31897)
+const script = async (course) => {
+  try {
+    const registrations = await getRegistrations(course)
+  } catch (error) {
+    console.log('Error:', error.message)
+  }
+}
+
+courseCodes.forEach((course) => {
+  script(course)
+})

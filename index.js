f0d39e6cf66293d6ffa169b9e3925bf5302bd838
Tero Tapio <tero.tapio@helsinki.fi>
2019-05-16 10:21:41 +0300

    add saving to local database

--- index.js
+++ index.js
@@ -13,15 +13,13 @@ const script = async (course) => {
     const credits = await db.credits.findAll({ raw: true })
     const moocIdsInDb = credits.map((credit) => credit.moocId)
     const studentIdsInDb = credits.map((credit) => credit.studentId)
-    //console.log('moocId:', moocIdsInDb)
-    //console.log('studentId:', studentIdsInDb)
 
     const registrations = await getRegistrations(course)
     const completions = await getCompletions(course)
-    //console.log('registration:', registrations[0])
-    //console.log('completion:', completions[0])
+
     const filteredRegistrations = registrations.filter(
-      (registration) => !studentIdsInDb.includes(registration.onro)
+      (registration) =>
+        registration.onro && !studentIdsInDb.includes(registration.onro)
     )
     console.log('Filtered registrations:', filteredRegistrations.length)
     const filteredCompletions = completions.filter(
@@ -29,7 +27,6 @@ const script = async (course) => {
     )
     console.log('Filtered completions:', filteredCompletions.length)
 
-    console.log('starting matching')
     let matchesFi = []
     let matchesEn = []
     for (const registration of filteredRegistrations) {
@@ -44,15 +41,19 @@ const script = async (course) => {
               ...rest,
               moocId: id,
               studentId: registration.onro,
-              courseId: course
+              courseId: course,
+              isInOodikone: false
             })
           } else if (completion_language === 'en_us') {
             matchesEn = matchesEn.concat({
               ...rest,
               moocId: id,
               studentId: registration.onro,
-              courseId: course
+              courseId: course,
+              isInOodikone: false
             })
+          } else {
+            console.log('unknown language')
           }
         }
       }
@@ -64,37 +65,58 @@ const script = async (course) => {
     console.log(
       `Found ${matchesEn.length} matches for english course ${course}.`
     )
-    const dateNow =  new Date()
+    const dateNow = new Date()
 
-    const date = `${dateNow.getDate()}.${dateNow.getMonth() + 1}.${dateNow.getFullYear()}`
-    const shortDate = `${dateNow.getDate()}.${dateNow.getMonth() + 1}.${dateNow.getYear()}`
-    
-    const reportEn = matchesEn.map((entry) => 
-      `${entry.studentId}##6#AYTKT21018#The Elements of AI#${date}#0#Hyv.#106##${TEACHERCODE}#1#H930#11#93013#3##2,0`
-    ).join('\n')
+    const date = `${dateNow.getDate()}.${dateNow.getMonth() +
+      1}.${dateNow.getFullYear()}`
+    const shortDate = `${dateNow.getDate()}.${dateNow.getMonth() +
+      1}.${dateNow.getYear()}`
 
-    const reportFi = matchesFi.map((entry) => 
-      `${entry.studentId}##1#AYTKT21018fi#Elements of AI: Tekoälyn perusteet#${date}#0#Hyv.#106##${TEACHERCODE}$#1#H930#11#93013#3##2,0`
-    ).join('\n')
+    const reportEn = matchesEn
+      .map(
+        (entry) =>
+          `${
+            entry.studentId
+          }##6#AYTKT21018#The Elements of AI#${date}#0#Hyv.#106##${TEACHERCODE}#1#H930#11#93013#3##2,0`
+      )
+      .join('\n')
 
-    fs.writeFile(`reports/AYTKT21018%${shortDate}-V1-S2019.dat`, reportEn, (err) => {
-      if (err) throw err;
-    });
+    const reportFi = matchesFi
+      .map(
+        (entry) =>
+          `${
+            entry.studentId
+          }##1#AYTKT21018fi#Elements of AI: Tekoälyn perusteet#${date}#0#Hyv.#106##${TEACHERCODE}#1#H930#11#93013#3##2,0`
+      )
+      .join('\n')
 
-    fs.writeFile(`reports/AYTKT21018fi%${shortDate}-V1-S2019.dat`, reportFi, (err) => {
-      if (err) throw err;
-    });
+    fs.writeFile(
+      `reports/AYTKT21018%${shortDate}-V1-S2019.dat`,
+      reportEn,
+      (err) => {
+        if (err) throw err
+      }
+    )
 
-    /*     console.log('Example: ', matches[0])
-    console.log(
-      'Example has oodi entry:',
-      await hasOodiEntry(matches[0].studentId, course)
-    ) */
+    fs.writeFile(
+      `reports/AYTKT21018fi%${shortDate}-V1-S2019.dat`,
+      reportFi,
+      (err) => {
+        if (err) throw err
+      }
+    )
+
+    matchesEn.forEach((entry) => {
+      db.credits.create(entry)
+    })
+
+    matchesFi.forEach((entry) => {
+      db.credits.create(entry)
+    })
   } catch (error) {
     console.log('Error:', error.message)
   }
 }
-
 
 courseCodes.forEach((course) => {
   script(course)

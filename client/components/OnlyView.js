183e1bcf5513d0544b1a74ac6787d4c8f54c5b3a
Tero Tapio <tero.tapio@helsinki.fi>
2019-06-07 10:16:10 +0300

    adds ReportDisplay

--- client/components/OnlyView.js
+++ client/components/OnlyView.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import { Button } from 'semantic-ui-react'
 
 import Dropzone from 'Components/Dropzone'
+import ReportDisplay from 'Components/ReportDisplay'
 
 export default () => {
-  const [reportData, setReportData] = useState('')
+  const [reportData, setReportData] = useState('No report given.')
 
   return (
     <div>
       <br />
       <Dropzone setReportData={setReportData} />
-      <p>{reportData}</p>
+      <ReportDisplay reportData={reportData} />
     </div>
   )
 }
---
+++ client/components/ReportDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const parseDataToReport = (data) => {
+  const report = data
+  return report
+}
+
+export default ({ reportData }) => <p>{parseDataToReport(reportData)}</p>

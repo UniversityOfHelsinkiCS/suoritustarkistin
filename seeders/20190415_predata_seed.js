2463ed62c9084d5de24e4ce340a0b64bd8e006f6
Jere Helén <jere.helen@helsinki.fi>
2019-06-25 20:44:01 +0300

    Fix seed file
    Seed file failed if the text file it refers was missing.
    Changed seed to use mock data if text file is missing.

--- seeders/20190415_predata_seed.js
+++ seeders/20190415_predata_seed.js
@@ -4,18 +4,51 @@ const fs = require('fs')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    const rawData = fs.readFileSync('sids.txt', 'utf8')
-    const oldList = rawData.split('\n').map((sid) => ({
-      studentId: sid,
-      courseId: 'AYTKT21018',
-      moocId: -1,
-      isInOodikone: false,
-      createdAt: Sequelize.fn('NOW'),
-      updatedAt: Sequelize.fn('NOW'),
-      reportId: 1
-    }))
-    console.log('# of entries', oldList.length)
-    return queryInterface.bulkInsert('credits', oldList, {})
+    if (fs.existsSync('sids.txt')) {
+      const rawData = fs.readFileSync('sids.txt', 'utf8')
+      const oldList = rawData.split('\n').map((sid) => ({
+        studentId: sid,
+        courseId: 'AYTKT21018',
+        moocId: -1,
+        isInOodikone: false,
+        createdAt: Sequelize.fn('NOW'),
+        updatedAt: Sequelize.fn('NOW'),
+        reportId: 1
+      }))
+      console.log('# of entries', oldList.length)
+      return queryInterface.bulkInsert('credits', oldList, {})
+    } else {
+      const mockList = [
+        {
+          studentId: '010000003',
+          courseId: 'AYTKT21018',
+          moocId: 1,
+          isInOodikone: false,
+          createdAt: Sequelize.fn('NOW'),
+          updatedAt: Sequelize.fn('NOW'),
+          reportId: 1
+        },
+        {
+          studentId: '011000002',
+          courseId: 'AYTKT21018',
+          moocId: 2,
+          isInOodikone: false,
+          createdAt: Sequelize.fn('NOW'),
+          updatedAt: Sequelize.fn('NOW'),
+          reportId: 1
+        },
+        {
+          studentId: '011100009',
+          courseId: 'AYTKT21018',
+          moocId: 3,
+          isInOodikone: false,
+          createdAt: Sequelize.fn('NOW'),
+          updatedAt: Sequelize.fn('NOW'),
+          reportId: 1
+        }
+      ]
+      return queryInterface.bulkInsert('credits', mockList, {})
+    }
   },
 
   down: (queryInterface, Sequelize) => {

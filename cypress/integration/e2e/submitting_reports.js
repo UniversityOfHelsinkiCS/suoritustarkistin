3320d8d8d262e95ff8c536fbda8dec9a4ed481b1
Tero Tapio <tero.tapio@helsinki.fi>
2019-08-09 14:55:53 +0300

    remove colons from file name and installs moment

--- cypress/integration/e2e/submitting_reports.js
+++ cypress/integration/e2e/submitting_reports.js
@@ -129,7 +129,7 @@ describe('Submitting data creates a valid report into database', function() {
         }
       }).should((response) => {
         const { fileName, data } = response.body
-        expect(fileName).to.contain('TKTTEST%5.7.19-')
+        expect(fileName).to.contain('TKTTEST%')
         expect(fileName).to.contain('V1-S2019.dat')
         expect(data).to.equal(
           '010000003##1#TKTTEST#tkt:n kurssi#5.7.2019#0#2#106##000000-000A#1#H523#####5,0\n011000002##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##000000-000A#1#H523#####2,0\n011100009##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##000000-000A#1#H523#####8,0\n011110002##1#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##000000-000A#1#H523#####8,0'
@@ -184,7 +184,7 @@ describe('Submitting data creates a valid report into database', function() {
         }
       }).should((response) => {
         const { fileName, data } = response.body
-        expect(fileName).to.contain('TKTTEST%5.7.19-')
+        expect(fileName).to.contain('TKTTEST%')
         expect(fileName).to.contain('-V1-S2019.dat')
         expect(data).to.equal(
           '010000003##1#TKTTEST#tkt:n kurssi#5.7.2019#0#2#106##000000-000A#1#H523#####5,0\n011000002##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##000000-000A#1#H523#####2,0\n011100009##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##000000-000A#1#H523#####8,0\n011110002##1#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##000000-000A#1#H523#####8,0'

9a6cb225602a33e45dd6d4756c8c8a0973d12673
Jere Helén <jere.helen@helsinki.fi>
2019-10-02 15:22:47 +0300

    Remove trailing slash from BASE_PATH

--- client/services/courses.js
+++ client/services/courses.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = `${__BASE_PATH__}api/courses` // BASE_PATH comes from webpack.config.js
+const baseUrl = `${__BASE_PATH__}/api/courses` // BASE_PATH comes from webpack.config.js
 
 const getAll = async () => {
   const courses = await axios.get(baseUrl)

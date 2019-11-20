4cb1f44fdab5792dad865190243c1d6a6e84f329
Jere Helén <jere.helen@helsinki.fi>
2019-11-20 14:52:49 +0200

    Make navbar responsive

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,54 +1,56 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Menu } from 'semantic-ui-react'
 import { useDispatch, useSelector } from 'react-redux'
 import { logoutAction } from 'Utilities/redux/userReducer'
 import { images } from 'Utilities/common'
 import { Link } from 'react-router-dom'
 
-const headerStyle = {
-  position: 'absolute',
-  top: '11px',
-  left: '50%',
-  transform: 'translate(-50%, -50%)'
-}
-
-const userStyle = {
-  margin: '16px',
-  float: 'right'
-}
-
 export default () => {
+  const [activeItem, setActiveItem] = useState('newReport')
   const dispatch = useDispatch()
   const user = useSelector((state) => state.user)
   const handleLogout = () => {
     dispatch(logoutAction())
   }
+
+  const handleItemClick = (e, { name }) => setActiveItem(name)
+
+  if (!user.data) return null
   return (
-    <div className="navbar">
-      <img src={images.toska_color} style={{ height: '100%' }} alt="tosca" />
-      <h1 style={headerStyle}>SUORITUSTARKISTIN</h1>
-      {user.data ? (
-        <div style={{ float: 'right' }}>
-          <div style={userStyle}>
-            <Button
-              content={`Log out ${user.data.name}`}
-              icon="sign-out"
-              labelPosition="right"
-              onClick={handleLogout}
-            />
-          </div>
-          <div style={userStyle}>
-            <Link to={'/reports'}>
-              <Button content={`View reports`} />
-            </Link>
-          </div>
-          <div style={userStyle}>
-            <Link to={'/'}>
-              <Button content={`New report`} />
-            </Link>
-          </div>
-        </div>
-      ) : null}
-    </div>
+    <Menu stackable size="huge" fluid>
+      <Menu.Item style={{ fontSize: 'xx-large', padding: '0.5em' }}>
+        <img
+          src={images.toska_color}
+          style={{ marginRight: '1em' }}
+          alt="tosca"
+        />{' '}
+        SUORITUSTARKISTIN
+      </Menu.Item>
+
+      <Menu.Item
+        position="right"
+        as={Link}
+        to={'/'}
+        name="newReport"
+        active={activeItem === 'newReport'}
+        onClick={handleItemClick}
+      >
+        New report
+      </Menu.Item>
+
+      <Menu.Item
+        as={Link}
+        to={'/reports'}
+        name="viewReports"
+        active={activeItem === 'viewReports'}
+        onClick={handleItemClick}
+      >
+        View reports
+      </Menu.Item>
+
+      <Menu.Item name="log-out" onClick={handleLogout}>
+        Log out
+      </Menu.Item>
+    </Menu>
   )
 }

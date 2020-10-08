d9aff2c88e35174c5c5935b9eaa3742219d4539d
Jere Helén <jere.helen@helsinki.fi>
2020-10-08 13:24:32 +0300

    Fix tests expecting old filenames

--- cypress/integration/e2e/submitting_reports.js
+++ cypress/integration/e2e/submitting_reports.js
@@ -1,9 +1,9 @@
-describe('Submitting data creates a valid report into database', function() {
-  beforeEach(function() {
+describe('Submitting data creates a valid report into database', function () {
+  beforeEach(function () {
     cy.server({
       onAnyRequest: (route, proxy) => {
         proxy.xhr.setRequestHeader('employeenumber', '321')
-      }
+      },
     })
     cy.request('DELETE', '/api/courses')
     cy.request('DELETE', '/api/users')
@@ -13,27 +13,27 @@ describe('Submitting data creates a valid report into database', function() {
       name: 'testiope',
       employeeId: '123',
       isAdmin: true,
-      isGrader: false
+      isGrader: false,
     })
     cy.request('POST', '/api/users', {
       name: 'testimaikka',
       employeeId: '321',
       isAdmin: false,
-      isGrader: true
+      isGrader: true,
     }).then((response) => {
       cy.request('POST', '/api/courses', {
         name: 'avoimen kurssi',
         courseCode: 'AYTKTTEST',
         language: 'fi',
         credits: '8,0',
-        graderId: response.body.id
+        graderId: response.body.id,
       })
       cy.request('POST', '/api/courses', {
         name: 'tkt:n kurssi',
         courseCode: 'TKTTEST',
         language: 'en',
         credits: '8,0',
-        graderId: response.body.id
+        graderId: response.body.id,
       })
     })
   })
@@ -43,12 +43,11 @@ describe('Submitting data creates a valid report into database', function() {
     cy.get('[data-cy=sendButton]').should('be.disabled')
     cy.get('[data-cy=pastefield]').type(
       '010000003;2;5;fi\n011000002;;2,0\n011100009\n011110002;;;fi',
-      { delay: 1 }
+      {
+        delay: 1,
+      }
     )
-    cy.get('[data-cy=dateField]')
-      .children()
-      .clear()
-      .type('5.7.2019')
+    cy.get('[data-cy=dateField]').children().clear().type('5.7.2019')
     cy.get('[data-cy=graderSelection]')
       .click()
       .children()
@@ -71,19 +70,19 @@ describe('Submitting data creates a valid report into database', function() {
     cy.request({
       url: 'api/reports/list',
       headers: {
-        Authorization: Cypress.env('SUOTAR_TOKEN')
-      }
+        Authorization: Cypress.env('SUOTAR_TOKEN'),
+      },
     }).should((response) => {
       expect(response.body.length).to.equal(1)
       cy.request({
         url: `api/reports/${response.body[0].id}`,
         headers: {
-          Authorization: Cypress.env('SUOTAR_TOKEN')
-        }
+          Authorization: Cypress.env('SUOTAR_TOKEN'),
+        },
       }).should((response) => {
         const { fileName, data } = response.body
         expect(fileName).to.contain('AYTKTTEST%')
-        expect(fileName).to.contain('-V1-S2019.dat')
+        expect(fileName).to.contain('_MANUAL.dat')
         expect(data).to.equal(
           '010000003##1#AYTKTTEST#avoimen kurssi#5.7.2019#0#2#106##321#2#H930#11#93013#3##5,0\n011000002##1#AYTKTTEST#avoimen kurssi#5.7.2019#0#Hyv.#106##321#2#H930#11#93013#3##2,0\n011100009##1#AYTKTTEST#avoimen kurssi#5.7.2019#0#Hyv.#106##321#2#H930#11#93013#3##8,0\n011110002##1#AYTKTTEST#avoimen kurssi#5.7.2019#0#Hyv.#106##321#2#H930#11#93013#3##8,0'
         )
@@ -96,12 +95,11 @@ describe('Submitting data creates a valid report into database', function() {
     cy.get('[data-cy=sendButton]').should('be.disabled')
     cy.get('[data-cy=pastefield]').type(
       '010000003;2;5;fi\n011000002;;2,0\n011100009\n011110002;;;fi',
-      { delay: 1 }
+      {
+        delay: 1,
+      }
     )
-    cy.get('[data-cy=dateField]')
-      .children()
-      .clear()
-      .type('5.7.2019')
+    cy.get('[data-cy=dateField]').children().clear().type('5.7.2019')
     cy.get('[data-cy=graderSelection]')
       .click()
       .children()
@@ -124,19 +122,19 @@ describe('Submitting data creates a valid report into database', function() {
     cy.request({
       url: 'api/reports/list',
       headers: {
-        Authorization: Cypress.env('SUOTAR_TOKEN')
-      }
+        Authorization: Cypress.env('SUOTAR_TOKEN'),
+      },
     }).should((response) => {
       expect(response.body.length).to.equal(1)
       cy.request({
         url: `api/reports/${response.body[0].id}`,
         headers: {
-          Authorization: Cypress.env('SUOTAR_TOKEN')
-        }
+          Authorization: Cypress.env('SUOTAR_TOKEN'),
+        },
       }).should((response) => {
         const { fileName, data } = response.body
         expect(fileName).to.contain('TKTTEST%')
-        expect(fileName).to.contain('V1-S2019.dat')
+        expect(fileName).to.contain('_MANUAL.dat')
         expect(data).to.equal(
           '010000003##1#TKTTEST#tkt:n kurssi#5.7.2019#0#2#106##321#2#H523#####5,0\n011000002##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##321#2#H523#####2,0\n011100009##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##321#2#H523#####8,0\n011110002##1#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##321#2#H523#####8,0'
         )
@@ -152,9 +150,7 @@ describe('Submitting data creates a valid report into database', function() {
     cy.fixture('valid.csv').then((content) => {
       cy.get('[data-cy=dropzone]').upload(content, 'valid.csv')
     })
-    cy.get('[data-cy=dateField] input')
-      .clear()
-      .type('5.7.2019')
+    cy.get('[data-cy=dateField] input').clear().type('5.7.2019')
     cy.get('[data-cy=graderSelection]')
       .click()
       .children()
@@ -176,19 +172,19 @@ describe('Submitting data creates a valid report into database', function() {
     cy.request({
       url: 'api/reports/list',
       headers: {
-        Authorization: Cypress.env('SUOTAR_TOKEN')
-      }
+        Authorization: Cypress.env('SUOTAR_TOKEN'),
+      },
     }).should((response) => {
       expect(response.body.length).to.equal(1)
       cy.request({
         url: `api/reports/${response.body[0].id}`,
         headers: {
-          Authorization: Cypress.env('SUOTAR_TOKEN')
-        }
+          Authorization: Cypress.env('SUOTAR_TOKEN'),
+        },
       }).should((response) => {
         const { fileName, data } = response.body
         expect(fileName).to.contain('TKTTEST%')
-        expect(fileName).to.contain('-V1-S2019.dat')
+        expect(fileName).to.contain('_MANUAL.dat')
         expect(data).to.equal(
           '010000003##1#TKTTEST#tkt:n kurssi#5.7.2019#0#2#106##321#2#H523#####5,0\n011000002##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##321#2#H523#####2,0\n011100009##6#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##321#2#H523#####8,0\n011110002##1#TKTTEST#tkt:n kurssi#5.7.2019#0#Hyv.#106##321#2#H523#####8,0'
         )

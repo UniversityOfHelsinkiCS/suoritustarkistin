5e445e4e73d06e2fdca5e7a612937eda8023ca10
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-01-04 13:11:31 +0200

    Make sender nullable in entry and refactor has sent boolean to date

---
+++ migrations/20210104073207-allow-null-in-entry-sender-id.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('entries', 'senderId', {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    })
+    await queryInterface.changeColumn('entries', 'senderId', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+          model: 'users',
+          key: 'id'
+      }
+    })
+  },
+  down: () => {}
+};

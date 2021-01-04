62b6b7a3fa4611ce22ddfd127307a7b04564fdf9
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-01-04 13:13:16 +0200

    Fix tests

--- cypress/integration/e2e/sis_reports.js
+++ cypress/integration/e2e/sis_reports.js
@@ -13,7 +13,6 @@ describe('SIS Reports -page shows data correctly', () => {
 
     cy.get('[data-cy=sis-reports-tab]').click()
     cy.get('[data-cy=sis-report-TKT10001]').click()
-    cy.get('[data-cy=sis-entries-panel-TKT10001]').click()
 
     cy.get('[data-cy=sis-report-grade-1]').should('contain', 'valid grade 1')
 
@@ -45,15 +44,12 @@ describe('SIS Reports -page shows data correctly', () => {
 
     cy.get('[data-cy=sis-reports-tab]').click()
     cy.get('[data-cy=sis-report-TKT10001]').click()
-    cy.get('[data-cy=sis-entries-panel-TKT10001]').click()
     cy.get('[data-cy=sis-report-entry-delete-button-1]').should('be.visible').click()
 
     cy.get('[data-cy=sis-report-TKT10002]').click()
-    cy.get('[data-cy=sis-entries-panel-TKT10002]').click()
     cy.get('[data-cy=sis-report-entry-delete-button-2]').should('be.visible').click()
 
     cy.get('[data-cy=sis-report-TKT10002]').click()
-    cy.get('[data-cy=sis-entries-panel-TKT10002]').click()
     cy.get('[data-cy=sis-report-entry-delete-button-3]').should('be.visible').click()
 
     cy.get('[data-cy=sis-no-reports]')

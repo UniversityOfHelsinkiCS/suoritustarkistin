93a6fc07d3ee8a21bb613bac7e4224eefedae924
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-01-13 12:45:16 +0200

    Rewrite logger and add log to gelf

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,29 +1,40 @@
 const winston = require('winston')
+const Log2gelf = require('winston-log2gelf')
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    winston.format.json()
-  ),
-  //defaultMeta: { service: 'suoritustarkistin' },
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    // new winston.transports.File({ filename: 'combined.log' }),
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        winston.format.printf(
-          (info) => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-      )
+const { LOG_PORT, LOG_HOST, LOG_HOSTNAME, LOG_PATH, LOG_PROTOCOL, NODE_ENV } = process.env
+
+const transports = []
+
+if (LOG_PORT && LOG_HOST) {
+  transports.push(
+    new Log2gelf({
+      hostname: LOG_HOSTNAME || 'suotar',
+      host: LOG_HOST,
+      port: LOG_PORT,
+      protocol: LOG_PROTOCOL || 'https',
+      environment: NODE_ENV,
+      service: 'SUOTAR',
+      protocolOptions: {
+        path: LOG_PATH || '/gelf'
+      }
     })
-  ]
-})
+  )
+}
+
+transports.push(
+  new winston.transports.Console({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.json(),
+      winston.format.colorize(),
+      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      winston.format.printf(
+        (info) => `${info.timestamp} ${info.level}: ${info.message}`
+      )
+    )
+  })
+)
+
+const logger = winston.createLogger({ transports })
 
 module.exports = logger

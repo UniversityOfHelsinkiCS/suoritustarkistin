2f14dbd7975d73e27a788aebb4029944a58206d4
Saara Satokangas <saarasat@gmail.com>
2021-02-01 09:48:41 +0200

    Add confirmation modal to sending sis-reports

--- client/components/NewReportPage/SisSendButton.js
+++ client/components/NewReportPage/SisSendButton.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Button, Popup } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Header, Modal, Popup, Segment } from 'semantic-ui-react'
 import { useSelector, useDispatch } from 'react-redux'
 import { sendNewRawEntriesAction } from 'Utilities/redux/sisNewRawEntriesReducer'
 import { sisAreValidNewRawEntries } from 'Root/utils/validators'
@@ -25,32 +25,68 @@ const parseRawEntries = (rawEntries) => {
 } 
 
 export default () => {
+  const [showForm, setShowForm] = useState(false)
   const dispatch = useDispatch()
   const newRawEntries = useSelector((state) => state.newRawEntries)
+  const courses = useSelector((state) => state.courses.data)
+
+  const closeModal = () => setShowForm(false)
+
+  if (!courses) return null
+
+  const course = courses.find((c) => c.id === newRawEntries.courseId)
 
   const sendRawEntries = () => {
     dispatch(sendNewRawEntriesAction(newRawEntries))
+    closeModal()
   }
 
   return (
-    <Popup
-    trigger={
-      <span style={{ float: 'right' }}>
-        <Button
-          positive
-          data-cy="sisSendButton"
-          onClick={sendRawEntries}
-          disabled={
-            newRawEntries.sending || !sisAreValidNewRawEntries(parseRawEntries(newRawEntries))
+    <>
+    <Modal
+      basic
+      inverted
+      open={showForm}
+      onClose={closeModal}
+      size="small"
+    >
+      <Modal.Content >
+      <Segment style={{ height: "15em", textAlign: "center", padding: "2em"}}>
+        <Header>
+          <p>
+            {newRawEntries.data ? newRawEntries.data.length : 0} completion(s) will be reported for the course:</p>
+          <p>{course
+            ? `${course.courseCode}: ${course.name}`
+            : <span style={{ color:"grey" }}>No course chosen yet</span>
           }
-          content="Send report"
-        />
-      </span>
-    }
-    content="Report contains validation errors, see table below."
-    disabled={!newRawEntries.data || sisAreValidNewRawEntries(parseRawEntries(newRawEntries))}
-    style={{ color: 'red' }}
-  />
+          </p>
+        </Header>
+        <div style={{ marginTop: "2em" }}>
+          <Button color="green" onClick={sendRawEntries}>Create report</Button>
+          <Button onClick={closeModal}>Cancel</Button>
+        </div>
+      </Segment>
 
+      </Modal.Content>
+    </Modal>
+    <Popup
+      trigger={
+        <span style={{ float: 'right' }}>
+          <Button
+            positive
+            data-cy="sisSendButton"
+            onClick={() => setShowForm(true)}
+            disabled={
+              newRawEntries.sending || !sisAreValidNewRawEntries(parseRawEntries(newRawEntries))
+            }
+            content="Create report"
+          />
+        </span>
+      }
+      content="Report contains validation errors, see table below."
+      disabled={!newRawEntries.data || sisAreValidNewRawEntries(parseRawEntries(newRawEntries))}
+      style={{ color: 'red' }}
+    />
+  </>
   )
 }

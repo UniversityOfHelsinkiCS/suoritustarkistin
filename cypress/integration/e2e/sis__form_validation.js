649bd16814d62ecf83daa8681509165d8c1ab57b
Saara Satokangas <saarasat@gmail.com>
2021-02-01 10:18:21 +0200

    Fix tests

--- cypress/integration/e2e/sis__form_validation.js
+++ cypress/integration/e2e/sis__form_validation.js
@@ -29,7 +29,7 @@ describe('SIS form validation', () => {
         .contains('Valid course 3 (TKT10003)')
         .click()
   
-      cy.get('[data-cy=sisSendButton]').should('be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('be.disabled')
     })
   
     it('when there are missing fields', () => {
@@ -37,7 +37,7 @@ describe('SIS form validation', () => {
       cy.asAdmin().visit('')
       cy.get('[data-cy=adminmode-enable]').click()
       cy.get('[data-cy=sis-copypaste]').should('be.visible').click()
-      cy.get('[data-cy=sisSendButton]').should('be.disabled')  
+      cy.get('[data-cy=sis-create-report-button]').should('be.disabled')  
   
       // missing course
       cy.get('[data-cy=sisPastefield]').type(
@@ -48,37 +48,37 @@ describe('SIS form validation', () => {
         .clear()
         .type('12.7.2020')
   
-      cy.get('[data-cy=sisSendButton]').should('be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('be.disabled')
       cy.get('[data-cy=sisGraderSelection]')
         .click()
         .children()
         .contains('grader')
         .click()
-      cy.get('[data-cy=sisSendButton]').should('be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('be.disabled')
       cy.get('[data-cy=sisCourseSelection]')
         .click()
         .children()
         .contains('Valid course 1 (TKT10001)')
         .click()
-      cy.get('[data-cy=sisSendButton]').should('not.be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('not.be.disabled')
   
       // missing data
       cy.get('[data-cy=sisPastefield]').clear()
-      cy.get('[data-cy=sisSendButton]').should('be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('be.disabled')
       cy.get('[data-cy=sisPastefield]').type(
         '010000003;2;5;fi\n011000002;;2,0\n011100009\n011110002;;;fi',
         { delay: 1 }
       )
-      cy.get('[data-cy=sisSendButton]').should('not.be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('not.be.disabled')
   
       // missing date
       cy.get('#sisDatePicker')
         .clear()
-      cy.get('[data-cy=sisSendButton]').should('be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('be.disabled')
       cy.get('#sisDatePicker')
         .clear()
         .type('21.21.2021')
-      cy.get('[data-cy=sisSendButton]').should('not.be.disabled')
+      cy.get('[data-cy=sis-create-report-button]').should('not.be.disabled')
     })  
   })  
 })

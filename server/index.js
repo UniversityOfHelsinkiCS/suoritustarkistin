6f7cca0fc620fda5992a915575b56875befb9d1d
Saara Satokangas <saarasat@gmail.com>
2021-02-09 16:33:05 +0200

    Up the tempo in checking oodi-entries

--- server/index.js
+++ server/index.js
@@ -243,7 +243,7 @@ initializeDatabaseConnection()
         processTiTo()
       })
 
-      cron.schedule('30 12-18 * * *', () => {
+      cron.schedule('*/15 12-18 * * *', () => {
         const timestamp = now()
         logger.info(
           `${timestamp.toLocaleString()} node-cron: Checking oodi entries.`

082dc26c5146f01cd048d9cabef9406b981ec692
Saara Satokangas <saarasat@gmail.com>
2021-02-11 20:36:44 +0200

    Try pushing the rest of the confirmations through

--- server/index.js
+++ server/index.js
@@ -243,7 +243,7 @@ initializeDatabaseConnection()
         processTiTo()
       })
 
-      cron.schedule('*/15 18-19 * * 4', () => {
+      cron.schedule('*/15 18-20 * * 4', () => {
         const timestamp = now()
         logger.info(
           `${timestamp.toLocaleString()} node-cron: Checking oodi entries.`

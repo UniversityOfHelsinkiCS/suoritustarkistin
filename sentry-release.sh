2dfaa2c8a30f61c507f781ffb6968effd68689ea
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-02-22 16:00:11 +0200

    git push --no-verify

--- sentry-release.sh
+++ sentry-release.sh
@@ -3,9 +3,8 @@ export SENTRY_ORG=toska
 SENTRY_PROJECT=suotar
 
 
-curl -sL https://sentry.io/get-cli/ | bash
 SENTRY_RELEASE=$(cat /SENTRY_RELEASE)
 sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_RELEASE
 sentry-cli releases set-commits --auto $SENTRY_RELEASE
-sentry-cli releases files $SENTRY_RELEASE upload ./dist/main.js '~/main.js'
+sentry-cli releases files $SENTRY_RELEASE upload ./dist/main.js '/suoritustarkistin/main.js'
 sentry-cli releases finalize $SENTRY_RELEASE

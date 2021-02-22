f9637f4fc56a6f77985549ed9055bf48347eb6d3
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-02-22 18:26:29 +0200

    Finishing touches

--- client/components/SandboxPage/index.js
+++ client/components/SandboxPage/index.js
@@ -13,7 +13,7 @@ export default () => {
   return (
     <Segment textAlign="center">
       { process.env.NODE_ENV !== "development"
-        ? <Header size="medium">Frontend built at ${process.env.BUILT_AT || 'unknown'}</Header>
+        ? <Header size="medium">Frontend built at {process.env.BUILT_AT || 'unknown'}</Header>
         : null}
       <Button size="huge" basic color="red" content="Chaos Monkey" icon="bomb" onClick={() => setCrash(true)} />
     </Segment>

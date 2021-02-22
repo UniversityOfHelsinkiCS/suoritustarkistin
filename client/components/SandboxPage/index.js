e33e916073fc119e805f0e82321f8b739087bb06
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-02-22 17:10:32 +0200

    Add built at timestamp

--- client/components/SandboxPage/index.js
+++ client/components/SandboxPage/index.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import { Segment, Button } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react'
+import { Segment, Button, Header } from 'semantic-ui-react'
 
 export default () => {
   const [crash, setCrash] = useState(false)
@@ -12,6 +12,9 @@ export default () => {
 
   return (
     <Segment textAlign="center">
+      { process.env.NODE_ENV !== "development"
+        ? <Header size="medium">Frontend built at ${process.env.BUILT_AT || 'unknown'}</Header>
+        : null}
       <Button size="huge" basic color="red" content="Chaos Monkey" icon="bomb" onClick={() => setCrash(true)} />
     </Segment>
   )
--- sentry-release.sh
+++ sentry-release.sh
@@ -6,5 +6,5 @@ SENTRY_PROJECT=suotar
 SENTRY_RELEASE=$(cat /SENTRY_RELEASE)
 sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_RELEASE
 sentry-cli releases set-commits --auto $SENTRY_RELEASE
-sentry-cli releases files $SENTRY_RELEASE upload ./dist/main.js '/suoritustarkistin/main.js'
+sentry-cli releases files $SENTRY_RELEASE upload ./dist/main.js '~/suoritustarkistin/main.js'
 sentry-cli releases finalize $SENTRY_RELEASE

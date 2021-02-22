a371b90488c91e9ead575a59592614b53ff135aa
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-02-22 14:24:58 +0200

    CI/CD is hard

--- sentry-release.sh
+++ sentry-release.sh
@@ -2,12 +2,10 @@
 export SENTRY_ORG=toska
 SENTRY_PROJECT=suotar
 
-cat ./dist/main.js
-
 
 curl -sL https://sentry.io/get-cli/ | bash
 SENTRY_RELEASE=$(sentry-cli releases propose-version)
 sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_RELEASE
 sentry-cli releases set-commits --auto $SENTRY_RELEASE
-sentry-cli releases files $SENTRY_RELEASE upload /dist/main.js '~/main.js'
+sentry-cli releases files $SENTRY_RELEASE upload ./dist/main.js '~/main.js'
 sentry-cli releases finalize $SENTRY_RELEASE

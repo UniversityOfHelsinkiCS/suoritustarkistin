18972855c60afa622bd4d1a246f7426743a7ecce
Saara Satokangas <saarasat@gmail.com>
2021-02-23 10:53:37 +0200

    Add uid-field to users-table

---
+++ migrations/20210223084822-add-uid-field-to-users.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('users', 'uid', {
+      allowNull: true,
+      type: Sequelize.STRING,
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('users', 'uid')
+  }
+}
--- server/models/users.js
+++ server/models/users.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define(
     'users',
     {
+      uid: DataTypes.STRING,
       name: DataTypes.STRING,
       employeeId: DataTypes.STRING,
       email: DataTypes.STRING,

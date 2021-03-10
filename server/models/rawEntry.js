b6ea573c27d9fa4fb1b86cb9609178fcc61b73fa
Saara Satokangas <saarasat@gmail.com>
2021-03-10 17:02:52 +0200

    Enable null for courseId in rawEntry

--- server/models/rawEntry.js
+++ server/models/rawEntry.js
@@ -38,7 +38,7 @@ module.exports = (sequelize, DataTypes) => {
     RawEntry.hasOne(models.entries, {foreignKey: 'rawEntryId', as: 'entry', onDelete:'CASCADE', onUpdate:'CASCADE'})
     RawEntry.belongsTo(models.users, {foreignKey: 'reporterId', as: 'reporter'})
     RawEntry.belongsTo(models.users, {foreignKey: 'graderId', as: 'grader'})
-    RawEntry.belongsTo(models.courses, {foreignKey: 'courseId', as: 'course'})
+    RawEntry.belongsTo(models.courses, {foreignKey: 'courseId', as: 'course', onDelete:'SET NULL'})
   }
   return RawEntry
 }

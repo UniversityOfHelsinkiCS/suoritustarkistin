512072cb642eb60cfee2b0344d1f2b06df2df2a3
Saara Satokangas <saarasat@gmail.com>
2021-03-11 12:35:25 +0200

    Remove duplicate foreign keys

---
+++ migrations/20210310084936-remove-duplicate-foreignkeys-from-raw-entries-and-entries.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.removeConstraint('entries', 'entries_senderId_fkey1', { transaction })
+      await queryInterface.removeConstraint('raw_entries', 'raw_entries_reporterId_fkey1', { transaction })
+      await transaction.commit()  
+    } catch (error) {
+      await transaction.rollback()
+      throw new Error(error)
+    }
+  },
+  down: async () => {}
+};

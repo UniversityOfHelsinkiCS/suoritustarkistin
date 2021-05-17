1e5403c28088d838bfe3ee3bafe9698d81b37621
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-05-17 12:21:52 +0300

    Refactor email sending in manual entry

--- server/controllers/sisRawEntryController.js
+++ server/controllers/sisRawEntryController.js
@@ -1,6 +1,9 @@
 const logger = require('@utils/logger')
 const db = require('../models/index')
 const { processManualEntry } = require('../scripts/sisProcessManualEntry')
+const sendEmail = require('../utils/sendEmail')
+const { newReport } = require('../utils/emailFactory')
+
 const handleDatabaseError = (res, error) => {
   logger.error(error.message)
   return res.status(500).json({ error: error.toString() })
@@ -16,7 +19,7 @@ const addRawEntries = async (req, res) => {
 
     const { courseId, graderId, date, data } = req.body
     if (!courseId || !graderId || !date || !data) {
-      logger.error({message: 'Unsuccessful upload: missing form fields', user: req.user.name, courseId, graderId, date, sis: true})
+      logger.error({ message: 'Unsuccessful upload: missing form fields', user: req.user.name, courseId, graderId, date, sis: true })
       return res.status(400).json({ error: 'invalid form values' })
     }
 
@@ -30,16 +33,36 @@ const addRawEntries = async (req, res) => {
 
     if (result.message === "success") {
       await transaction.commit()
-      logger.info({ message: 'Report of new completions created successfully.', sis: true})
+      logger.info({ message: 'Report of new completions created successfully.', sis: true })
+      const unsent = await db.entries.getUnsentBatchCount()
+      if (await shouldSendEmail(result.batchId))
+        sendEmail({
+          subject: `Uusia kurssisuorituksia: ${result.courseCode}`,
+          attachments: [{
+            filename: 'suotar.png',
+            path: `${process.cwd()}/client/assets/suotar.png`,
+            cid: 'toskasuotarlogoustcid'
+          }],
+          html: newReport(result.success.length, unsent, result.courseCode, result.batchId)
+        })
       return res.status(200).json({ message: 'report created successfully' })
     } else {
       await transaction.rollback()
-      logger.error({ message: `Processing new completions failed`, sis: true})
+      logger.error({ message: `Processing new completions failed`, sis: true })
       return res.status(400).json({ message: "Processing new completions failed", failed: result.failed })
     }
   } catch (error) {
     handleDatabaseError(res, error)
   }
+}
+
+async function shouldSendEmail(batchId) {
+  const rawEntries = await db.raw_entries.findAll({
+    where: { batchId },
+    include: [{ model: db.entries, as: 'entry' }],
+    raw: true
+  })
+  return rawEntries.some(({ entry }) => !entry.missingEnrolment)
 }
 
 module.exports = {

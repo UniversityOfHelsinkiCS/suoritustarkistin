94d263476abb69af2ceadf9e7547d892600eed59
Saara Satokangas <saarasat@gmail.com>
2021-05-25 19:13:06 +0300

    Add inProduction-restriction to sending back to Kurki

--- server/services/kurki.js
+++ server/services/kurki.js
@@ -34,15 +34,20 @@ const getCompletions = async (kurkiId) => {
 }
 
 const postTransferredId = async (kurkiId) => {
-  logger.info({ message: `Posting transferred course back to Kurki`})
-  const result = await kurkiApi.post(`/${kurkiId}/students-transferred`)
+  if (process.env.NODE_ENV === 'production') {
+    logger.info({ message: `Posting transferred course back to Kurki`})
+    const result = await kurkiApi.post(`/${kurkiId}/students-transferred`)
 
-  if (result.status === 200) {
-    logger.info({ message: `Posting successful with status code ${result.status}` })
-    return { message: "success" }
+    if (result.status === 200) {
+      logger.info({ message: `Posting successful with status code ${result.status}` })
+      return { message: "success" }
+    }
+
+    return { message: "Failed posting transferral to Kurki" }    
   }
 
-  return { message: "Failed posting transferral to Kurki" }
+  logger.info({ message: 'Not sent to Kurki, the environment is not production' })
+
 }
 
 module.exports = { getCourses, getCompletions, postTransferredId }

9822e169ed7eccb23b96fd2ba35e5633fb633665
Saara Satokangas <saarasat@gmail.com>
2021-05-31 13:40:39 +0300

    Remove unnecessary line

--- client/components/ReportsPage/SisReports/ReportTable.js
+++ client/components/ReportsPage/SisReports/ReportTable.js
@@ -30,12 +30,7 @@ const TableColumns = ({ allowDelete }) => (
       <Table.HeaderCell>Course name</Table.HeaderCell>
       <Table.HeaderCell>Student number</Table.HeaderCell>
       <Table.HeaderCell>Credits</Table.HeaderCell>
-      <Table.HeaderCell
-        style={{ borderLeft: "2px solid gray" }}
-        colSpan='2'
-      >
-        Course Unit
-      </Table.HeaderCell>
+      <Table.HeaderCell>Course Unit</Table.HeaderCell>
       <Table.HeaderCell>Student ID</Table.HeaderCell>
       <Table.HeaderCell>Completion date</Table.HeaderCell>
       <Table.HeaderCell>Language</Table.HeaderCell>
@@ -103,7 +98,6 @@ const EntryCells = ({ entry }) => {
       <Table.Cell
         data-cy={`sis-report-courseUnitRealisationName-${entry.id}`}
         colSpan='2'
-        style={{ borderLeft: "2px solid gray" }}
       >
         <Accordion className="sis-report-table-accordion" >
           <Accordion.Title

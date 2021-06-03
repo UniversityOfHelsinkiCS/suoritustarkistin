aa7fff18f18f6abac0616a1705fc7583a427d1fd
Saara Satokangas <saarasat@gmail.com>
2021-06-03 09:56:54 +0300

    Add gradeScale to course model

---
+++ migrations/20210602055246-add-grade-scale-to-courses.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('courses', 'gradeScale', {
+      allowNull: true,
+      type: Sequelize.STRING,
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('courses', 'gradeScale')
+  }
+}

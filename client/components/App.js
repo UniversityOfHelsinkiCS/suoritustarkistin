be6e83fa46e94105d8d9e6aa06df89e107465385
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-06-03 08:28:29 +0300

    Fix tests

--- client/components/App.js
+++ client/components/App.js
@@ -16,7 +16,7 @@ export default withRouter(({history}) => {
   const [redirected, setRedirected] = useState(false)
 
   useEffect(() => {
-    if(user.data.isAdmin && !redirected && process.env.NODE_ENV === 'production') {
+    if(user && user.data && user.data.isAdmin && !redirected && process.env.NODE_ENV === 'production') {
       setRedirected(true)
       history.push('/reports')
     }

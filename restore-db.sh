b8f732b5788825e32f81aebdfcdc36cfc7c9301b
Roni Lindholm <roni.lindholm@helsinki.fi>
2021-06-03 09:48:13 +0300

    Add unsent labels

--- restore-db.sh
+++ restore-db.sh
@@ -4,9 +4,14 @@ if [ -z "$1" ]
     exit
 fi
 
+docker-compose down
+docker-compose up -d db
+
 echo "Drop current db"
 docker-compose exec db dropdb -U postgres postgres
 echo "Create new empty db"
 docker-compose exec db createdb -U postgres postgres
 echo "Restore db from $1"
 docker-compose exec -T db psql -U postgres < "$1"
+
+npm run dev

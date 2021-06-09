a0265f448bb6d4cdf98331a0f54751afe60de017
Saara Satokangas <saarasat@gmail.com>
2021-06-09 09:47:02 +0300

    Change checkRegisteredForMooc to apply the new registered-field

--- server/scripts/checkSisEntries.js
+++ server/scripts/checkSisEntries.js
@@ -77,7 +77,7 @@ const checkRegisteredForMooc = async () => {
 
     const completionStudentPairs = unregistered.reduce(
       (completionStudentPairs, rawEntry) => {
-        const alreadyInSis = rawEntry.entry && rawEntry.entry.registered
+        const alreadyInSis = rawEntry.entry && (rawEntry.entry.registered === 'PARTLY_REGISTERED' || rawEntry.entry.registered === 'REGISTERED') 
         if (alreadyInSis) {
           return completionStudentPairs.concat({
             completion_id: rawEntry.moocCompletionId,

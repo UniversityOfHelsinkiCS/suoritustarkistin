3d943b9ea8e1170e6015948b3f88b52d96298892
Roni Lindholm <roni.lindholm@helsinki.fi>
2022-02-07 15:12:59 +0200

    Add Sentry url

--- sentry-release.sh
+++ sentry-release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export SENTRY_ORG=toska
-SENTRY_PROJECT=suotar
+export SENTRY_PROJECT=suotar
+export SENTRY_URL=https://sentry.cs.helsinki.fi
+export SENTRY_RELEASE=$(cat /SENTRY_RELEASE)
 
 if [[ -z "$SENTRY_AUTH_TOKEN" ]]; then
   echo "is set"
@@ -8,7 +10,6 @@ else
     echo "is not set"
 fi
 
-SENTRY_RELEASE=$(cat /SENTRY_RELEASE)
 sentry-cli releases new -p $SENTRY_PROJECT $SENTRY_RELEASE
 sentry-cli releases set-commits --auto $SENTRY_RELEASE
 sentry-cli releases files $SENTRY_RELEASE upload ./dist/main.js '~/suoritustarkistin/main.js'

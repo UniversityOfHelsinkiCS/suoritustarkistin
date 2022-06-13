abbe93c0b577f9b8fede391f7bf3c98e7f27baa0
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-06-13 13:47:22 +0300

    Add email to raw_entries migration

---
+++ migrations/20220613_01_add_email_to_raw_entries.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('raw_entries', 'email', {
+      type: Sequelize.STRING,
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('raw_entries', 'email')
+  }
+}

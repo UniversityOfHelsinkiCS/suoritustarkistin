74b0cd8a5703501fe3cb0ab34f1a9770731429a3
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-02-14 16:04:03 +0200

    Test different db connection

--- server/models/index.js
+++ server/models/index.js
@@ -12,6 +12,8 @@ const db = {}
 let sequelize
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
+} else if (env === 'staging') {
+  sequelize = new Sequelize(process.env.DATABASE_URL)
 } else {
   sequelize = new Sequelize(config.database_url, config)
 }

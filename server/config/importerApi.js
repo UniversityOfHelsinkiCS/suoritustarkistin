8622b674df05a8a707aba7f08308de6191cc40ea
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-05-31 17:14:25 +0300

    Increase Importer api timeout

--- server/config/importerApi.js
+++ server/config/importerApi.js
@@ -4,7 +4,8 @@ const api = axios.create({
   headers: {
     token: process.env.IMPORTER_DB_API_TOKEN || ''
   },
-  baseURL: process.env.IMPORTER_DB_API_URL
+  baseURL: process.env.IMPORTER_DB_API_URL,
+  timeout: 120_000
 })
 
 module.exports = api

41cfcf70f260888218edccf98f3db0c1fee33432
akskokki <akseli.kokki@gmail.com>
2026-07-31 12:04:04 +0300

    build: bump dependencies

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
   // eslint-disable-next-line global-require
-  require('dotenv').config()
+  require('dotenv').config({ quiet: true })
 }
 
 const OPTIONS = {

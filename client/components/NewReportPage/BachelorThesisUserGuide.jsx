1a7c94544e5b8331665b35a4113b23bc6d122373
akskokki <akseli.kokki@gmail.com>
2026-08-18 14:38:16 +0300

    refactor: extract shared DetailedInstructions component

--- client/components/NewReportPage/BachelorThesisUserGuide.jsx
+++ client/components/NewReportPage/BachelorThesisUserGuide.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import { List, ListItem, Paper, Tooltip, Typography } from '@mui/material'
+import { Paper, Tooltip, Typography } from '@mui/material'
 import HelpIcon from '@mui/icons-material/Help'
+import DetailedInstructions, { sharedInstructions } from './DetailedInstructions'
 
 const instructionContainer = {
   padding: '1rem'
 }
 
-const detailedInstructions = {
-  textAlign: 'left',
-  width: '60em'
-}
-
-const instruction = {
-  margin: '0 0 0.2em 0',
-  fontWeight: 700
-}
+const instructions = [
+  ...sharedInstructions,
+  {
+    title: 'Language of "Äidinkielinen viestintä"',
+    body: '"fi", "en", "sv". Use "x" to opt-out for student.'
+  },
+  {
+    title: 'Language of "Kypsyysnäyte"',
+    body: '"fi", "en", "sv". Use "x" to opt-out for student.'
+  },
+  {
+    title: 'Language of "Tutkimustiedonhaku"',
+    body: '"fi", "en", "sv". Use "x" to opt-out for student.'
+  }
+]
 
 const code = {
-  fontSize: '1ren',
+  fontSize: '1rem',
   padding: '2px 4px',
   color: '#1f1f1f',
   backgroundColor: '#f0f0f0',
@@ -29,52 +36,6 @@ const code2 = {
   maxWidth: '30rem',
   fontSize: '0.93rem'
 }
-
-const DetailedInstructions = () => (
-  <div style={detailedInstructions}>
-    <Typography variant="h5" component="h1" gutterBottom>
-      Detailed instructions
-    </Typography>
-    <List>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Student number</p>
-        Student numbers will be fetched from Sisu for reporting. Students not present in Sisu, cannot be given course
-        completions.
-      </ListItem>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Grade</p>
-        Each course has a pre-defined grade scale in Sisu. For most it is "0-5" or "Hyv.-Hyl.". Only grades within the
-        grade scale of the course can be given.
-      </ListItem>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Credits</p>
-        You can define the amount of credits for each student separately or use the course default credit amount.
-      </ListItem>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Language</p>
-        Suotar supports three languages for course completions "fi", "en", "sv".
-      </ListItem>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Date of completion</p>
-        You can add date separately for each student. Any date chosen from date-picker will apply to completions that do
-        not have a separately set date for them. Please note that the course instance will be picked based automatically
-        based on the completion date.
-      </ListItem>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Language of "Äidinkielinen viestintä"</p>
-        "fi", "en", "sv". Use "x" to opt-out for student.
-      </ListItem>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Language of "Kypsyysnäyte"</p>
-        "fi", "en", "sv". Use "x" to opt-out for student.
-      </ListItem>
-      <ListItem sx={{ display: 'block', px: 0, py: 0.5 }}>
-        <p style={instruction}>Language of "Tutkimustiedonhaku"</p>
-        "fi", "en", "sv". Use "x" to opt-out for student.
-      </ListItem>
-    </List>
-  </div>
-)
 
 export default () => (
   <Paper variant="outlined" data-cy="userguide" style={instructionContainer}>
@@ -92,7 +53,7 @@ export default () => (
     <Typography variant="h6" component="h3" sx={{ mt: 2, display: 'flex', alignItems: 'center' }}>
       Each completion should be its own line in the following format:
       <Tooltip
-        title={<DetailedInstructions />}
+        title={<DetailedInstructions instructions={instructions} />}
         slotProps={{
           tooltip: {
             sx: {
---
+++ client/components/NewReportPage/DetailedInstructions.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { List, ListItem, Typography } from '@mui/material'
+
+const container = {
+  textAlign: 'left',
+  width: '60em'
+}
+
+const title = {
+  margin: '0 0 0.2em 0',
+  fontWeight: 700
+}
+
+export const sharedInstructions = [
+  {
+    title: 'Student number',
+    body: 'Student numbers will be fetched from Sisu for reporting. Students not present in Sisu, cannot be given course completions.'
+  },
+  {
+    title: 'Grade',
+    body: 'Each course has a pre-defined grade scale in Sisu. For most it is "0-5" or "Hyv.-Hyl.". Only grades within the grade scale of the course can be given.'
+  },
+  {
+    title: 'Credits',
+    body: 'You can define the amount of credits for each student separately or use the course default credit amount.'
+  },
+  {
+    title: 'Language',
+    body: 'Suotar supports three languages for course completions "fi", "en", "sv".'
+  },
+  {
+    title: 'Date of completion',
+    body: 'You can add date separately for each student. Any date chosen from date-picker will apply to completions that do not have a separately set date for them. Please note that the course instance will be picked automatically based on the completion date.'
+  }
+]
+
+export default ({ instructions }) => (
+  <div style={container}>
+    <Typography variant="h5" component="h1" gutterBottom>
+      Detailed instructions
+    </Typography>
+    <List>
+      {instructions.map(({ title: heading, body }) => (
+        <ListItem key={heading} sx={{ display: 'block', px: 0, py: 0.5 }}>
+          <p style={title}>{heading}</p>
+          {body}
+        </ListItem>
+      ))}
+    </List>
+  </div>
+)
